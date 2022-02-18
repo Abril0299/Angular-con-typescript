a44bfef8337f5e343247c9240622daec3804e981
unknown <abril.hernandez.hernandez.02@gmail.com>
2022-02-17 20:30:34 -0600

    Practica Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContenidoComponent } from './componens/contenido/contenido.component';
+import { FooterComponent } from './componens/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContenidoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
